565e998c7aa6723c778a7716e159cfb6136af9f4
oscar pimentel <oscarlo.pimentel@gmail.com>
2020-12-30 15:54:02 -0300

    class

---
+++ fuzzytorch/models/attention/utils.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from __future__ import division
+from . import C_
+
+import torch
+import torch.nn as nn
+
+###################################################################################################################################################
