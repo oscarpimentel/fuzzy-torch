7e4e5465103be070c85f9360332b65301e5a3636
opimentel-deepspace <oscarlo.pimentel@gmail.com>
2021-08-07 12:17:29 -0400

    ablations

--- fuzzytorch/models/others.py
+++ fuzzytorch/models/others.py
@@ -231,12 +231,13 @@ class TimeFILM(nn.Module):
 
 	def reset(self):
 		self.dummy = self.te_features<=0
+		self.te_features = 2 if self.is_dummy() else self.te_features # patch
 		linear_kwargs = {
 			'activation':'linear',
 			#'bias':self.bias,
 			}
 		assert self.input_dims>0
-		self.temporal_encoder = TemporalEncoder(2 if self.is_dummy() else self.te_features, self.max_te_period,
+		self.temporal_encoder = TemporalEncoder(self.te_features, self.max_te_period,
 			time_noise_window=self.time_noise_window,
 			mod_dropout=self.mod_dropout,
 			)
